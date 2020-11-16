67ba377bc2ce9e87a97d9b1f9439be476b6446c5
nobodyatall648 <35725871+nobodyatall648@users.noreply.github.com>
2020-11-17 06:35:48 +0800

    Edit for loop condition

--- Capture The Flag (CTF)/APU Battle of Hacker 2020/NuclearWarfare/nuclearWeaponSol.ino
+++ Capture The Flag (CTF)/APU Battle of Hacker 2020/NuclearWarfare/nuclearWeaponSol.ino
@@ -19,7 +19,7 @@ void loop() {
   char buf[6];
   //nuclear launch code: 3073
   //print 0000-9999
-  for(int i=0; i < 9999; i++){        
+  for(int i=0; i < 10000; i++){        
     sprintf(buf, "%04d", i);
     Keyboard.print(buf);
     //typeKey(KEY_RETURN);
