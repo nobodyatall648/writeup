7ff4eafc86d369fb42b947ba1ec5cffd40e6bb96
nobodyatall648 <35725871+nobodyatall648@users.noreply.github.com>
2020-11-16 08:21:32 +0800

    Add files via upload

---
+++ Capture The Flag (CTF)/APU Battle of Hacker 2020/NuclearWarfare/nuclearWeaponSol.ino
@@ -0,0 +1,28 @@
+#include<Keyboard.h>
+
+void typeKey(int key){
+    Keyboard.press(key);
+    delay(50);
+    Keyboard.release(key);
+}
+
+void setup() {
+  Keyboard.begin();
+  delay(500);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int i=0;
+  char buf[6];
+  //nuclear launch code: 3073
+  //print 0000-9999
+  for(int i=0; i < 9999; i++){        
+    sprintf(buf, "%04d", i);
+    Keyboard.print(buf);
+    //typeKey(KEY_RETURN);
+    delay(30);      
+  }  
+}
